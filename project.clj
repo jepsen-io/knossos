74c43249defdfe5c2f027276960b47a4867471b9
Aphyr <aphyr@aphyr.com>
2013-12-22 20:37:13 -0800

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject knossos "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject knossos "0.1.0"
+  :description "Linearizability checker"
+  :url "https://github.com/aphyr/knossos"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
