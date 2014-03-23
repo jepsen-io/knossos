bae28a41c906c5365fad5eda5219a02d6b9f8c13
Aphyr <aphyr@aphyr.com>
2014-03-23 14:39:25 -0700

    Clojure 1.6

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :url "https://github.com/aphyr/knossos"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0-beta1"]
                  [org.clojure/math.combinatorics "0.0.7"]])
