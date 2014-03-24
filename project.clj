93de6898eb7ce273e72240d2c34684eb97ffe825
Aphyr <aphyr@aphyr.com>
2014-03-23 20:39:29 -0700

    Add cliff click's high scale lib

--- project.clj
+++ project.clj
@@ -3,8 +3,12 @@
   :url "https://github.com/aphyr/knossos"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :repositories {
+                 "boundary-site" "http://maven.boundary.com/artifactory/repo"
+                 }
   :dependencies [[org.clojure/clojure "1.6.0-beta1"]
                  [org.clojure/math.combinatorics "0.0.7"]
-                 [potemkin "0.3.4"]]
+                 [potemkin "0.3.4"]
+                 [com.boundary/high-scale-lib "1.0.3"]]
   ; "-verbose:gc" "-XX:+PrintGCDetails"
   :jvm-opts ["-Xmx32g" "-XX:+UseConcMarkSweepGC" "-XX:+UseParNewGC" "-XX:+CMSParallelRemarkEnabled" "-XX:+AggressiveOpts" "-XX:+UseFastAccessorMethods"])
