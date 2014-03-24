b5e5e8f3d42cc7603bed0d7e22d86b31f8d58ab6
Aphyr <aphyr@aphyr.com>
2014-03-23 19:52:51 -0700

    JVM/GC tuning

--- project.clj
+++ project.clj
@@ -5,4 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0-beta1"]
                  [org.clojure/math.combinatorics "0.0.7"]
-                 [potemkin "0.3.4"]])
+                 [potemkin "0.3.4"]]
+  ; "-verbose:gc" "-XX:+PrintGCDetails"
+  :jvm-opts ["-Xmx32g" "-XX:+UseConcMarkSweepGC" "-XX:+UseParNewGC" "-XX:+CMSParallelRemarkEnabled" "-XX:+AggressiveOpts" "-XX:+UseFastAccessorMethods" "-XX:+UseCompressedOops"])
