de6e014a52f598f771b98792a921b673e506e9e3
Aphyr <aphyr@aphyr.com>
2015-11-13 23:06:56 -0800

    array-processes-search: one more unchecked multiply

--- src/knossos/linear/config.clj
+++ src/knossos/linear/config.clj
@@ -166,7 +166,7 @@
        (dec (unchecked-multiply -2 low)) ; Because indices are bounded by
                                          ; MAX_INT / 2, this is safe.
        (let [mid     (quot (+ low high) 2)
-             mid-val (aget a (* 2 mid))]
+             mid-val (aget a (unchecked-multiply 2 mid))]
          (cond (< mid-val process)  (recur (inc mid)  high)
                (< process mid-val)  (recur low        (dec mid))
                true                 (unchecked-multiply 2 mid)))))))
