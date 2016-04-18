cac4cdddfed2437f02d6a39f0609893507a70008
Aphyr <aphyr@aphyr.com>
2016-04-18 12:31:03 -0500

    Clojure 1.8, tools.logging 0.4.1, slf4j 1.7.21, potemkin 0.4.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject knossos "0.2.4"
+(defproject knossos "0.2.5-SNAPSHOT"
   :description "Linearizability checker"
   :url "https://github.com/aphyr/knossos"
   :license {:name "Eclipse Public License"
@@ -6,13 +6,13 @@
   :repositories {"sonatype-oss-public"
                  "https://oss.sonatype.org/content/groups/public/"}
   :dependencies [[org.clojure/math.combinatorics "0.1.1"]
-                 [potemkin "0.3.4"]
+                 [potemkin "0.4.3"]
                  [interval-metrics "1.0.0"]
                  [com.boundary/high-scale-lib "1.0.6"]
                  [org.clojars.pallix/analemma "1.0.0"
                   :exclusions [org.clojure/clojure]]
-                 [org.clojure/tools.logging "0.2.6"]
-                 [org.slf4j/slf4j-log4j12 "1.6.1"]]
+                 [org.clojure/tools.logging "0.3.1"]
+                 [org.slf4j/slf4j-log4j12 "1.7.21"]]
   ; "-verbose:gc" "-XX:+PrintGCDetails"
   :test-selectors {:focus :focus}
   :jvm-opts ["-Xmx24g"
@@ -25,4 +25,4 @@
              "-XX:MaxRecursiveInlineLevel=2"
              "-XX:-OmitStackTraceInFastThrow"
              "-server"]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.7.0"]]}})
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.8.0"]]}})
