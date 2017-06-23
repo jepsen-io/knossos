2068b4ce7b9c9ceb15d4d8794e8b89b6c6eb1f22
Aphyr <aphyr@aphyr.com>
2017-06-23 16:24:11 -0500

    0.3.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject knossos "0.2.9-SNAPSHOT"
+(defproject knossos "0.3.0"
   :description "Linearizability checker"
   :url "https://github.com/aphyr/knossos"
   :license {:name "Eclipse Public License"
