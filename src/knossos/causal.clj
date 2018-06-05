96a59ea115f7e93e5296582833484d452e12ffea
Mikaela Patella <m.c.patella@gmail.com>
2018-06-05 15:52:39 -0700

    remove check for completed COs, we want to look for partial failure/success

--- src/knossos/causal.clj
+++ src/knossos/causal.clj
@@ -31,31 +31,26 @@
   [model history state]
   (let [attempted (filter op/invoke? history)
         completed (filter op/ok? history)]
-    (if-not (completed-co? attempted completed)
-      ;; This is to ensure a malformed CO does not invalidate a set of
-      ;; tests results.
-      {:valid? true
-       :cause "CO incomplete"}
-      (loop [s model
-             history completed]
-        (cond
-          ;; Manually aborted
-          (not (:running? @state))
-          {:valid? :unknown
-           :cause (:cause @state)}
+    (loop [s model
+           history completed]
+      (cond
+        ;; Manually aborted
+        (not (:running? @state))
+        {:valid? :unknown
+         :cause (:cause @state)}
 
-          ;; We've checked every operation in the history
-          (empty? history)
-          {:valid? true
-           :model s}
+        ;; We've checked every operation in the history
+        (empty? history)
+        {:valid? true
+         :model s}
 
-          true
-          (let [op (first history)
-                s' (model/step s op)]
-            (if (model/inconsistent? s')
-              {:valid? false
-               :error (:msg s')}
-              (recur s' (rest history)))))))))
+        true
+        (let [op (first history)
+              s' (model/step s op)]
+          (if (model/inconsistent? s')
+            {:valid? false
+             :error (:msg s')}
+            (recur s' (rest history))))))))
 
 (defn start-analysis
   "Spawns a search to check if a history is causally consistent"
