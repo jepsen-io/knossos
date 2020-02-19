dbf8279670a91ad01a94ab9d9ac5f0198500918b
Stevan Andjelkovic <stevana@users.noreply.github.com>
2020-02-19 16:43:25 +0100

    Add test case showing wgl analysis throws AbstractMethodError.

--- test/knossos/wgl_test.clj
+++ test/knossos/wgl_test.clj
@@ -1,10 +1,12 @@
 (ns knossos.wgl-test
   (:require [clojure.test :refer :all]
+            [clojure.spec.alpha :as s]
             [knossos.wgl :refer :all]
             [knossos.op :as op]
-            [knossos.model :refer [register cas-register inconsistent]]
+            [knossos.model :as model :refer [register cas-register inconsistent]]
             [knossos.core-test :as ct]
-            [clojure.pprint :refer [pprint]]))
+            [clojure.pprint :refer [pprint]])
+  (:import (knossos.model Model)))
 
 (deftest ^:perf timeout-test
   (testing "will timeout if limit exceeded"
@@ -84,8 +86,8 @@
 
 (deftest bad-analysis-test
   (let [a (analysis
-            (cas-register 0)
-            (ct/read-history-2 "data/cas-register/bad/bad-analysis.edn"))]
+           (cas-register 0)
+           (ct/read-history-2 "data/cas-register/bad/bad-analysis.edn"))]
     (is (= false (:valid? a)))
     (is (= {:process 21, :type :ok, :f :read, :value 2 :index 14} (:op a)))
     (is (= {:process 19, :type :ok, :f :read, :value 0 :index 11}
@@ -107,17 +109,17 @@
 
      ; The last linearized ok was the completion of process 74's read of 0, but
      ; that's not the last linearized *op*: that'd be the write of 2.
-     (is (= {:process 74, :type :ok, :f :read, :value 0 :index 478}
-            (:previous-ok a)))
+    (is (= {:process 74, :type :ok, :f :read, :value 0 :index 478}
+           (:previous-ok a)))
 
-     (is (= #{[{:model (cas-register 2)
-                :op {:f :write :process 76 :type :ok :value 2 :index 472}}
-               {:model (inconsistent "can't CAS 2 from 1 to 1")
-                :op {:f :cas :process 77 :type :info :value [1 1] :index 482}}]
-              [{:model (cas-register 2)
-                :op {:f :write :process 76 :type :ok :value 2 :index 472}}
-               {:model (inconsistent "can't read 0 from register 2")
-                :op {:f :read :process 70 :type :ok :value 0 :index 491}}]}
+    (is (= #{[{:model (cas-register 2)
+               :op {:f :write :process 76 :type :ok :value 2 :index 472}}
+              {:model (inconsistent "can't CAS 2 from 1 to 1")
+               :op {:f :cas :process 77 :type :info :value [1 1] :index 482}}]
+             [{:model (cas-register 2)
+               :op {:f :write :process 76 :type :ok :value 2 :index 472}}
+              {:model (inconsistent "can't read 0 from register 2")
+               :op {:f :read :process 70 :type :ok :value 0 :index 491}}]}
            (:final-paths a)))))
 
 (deftest rethink-fail-minimal-test
@@ -155,3 +157,135 @@
 
 (deftest ^:perf examples-test
   (ct/test-examples analysis))
+
+;; ---------------------------------------------------------------------
+
+(defrecord Log [entries])
+
+(defn topic? [x] (string? x))
+(defn data?  [x] (string? x))
+
+(s/def ::entries  (s/coll-of data? :kind vector?))
+(s/def ::log (s/keys :req-un [::entries]))
+
+(defn initial-log
+  []
+  {:post [(s/valid? ::log %)]}
+  (Log. (vector "network config"
+                "membership/add n2"
+                "membership/add n3"
+                "membership/add n4"
+                "membership/add n5")))
+
+(defn append
+  [log _topic _data]
+  {:pre  [(s/valid? ::log log) (s/valid? data? _data)]
+   :post [(s/valid? int? %)]}
+  (inc (count (:entries log))))
+
+(defn append-next
+  [log _topic data]
+  {:pre  [(s/valid? ::log log) (s/valid? string? data)]
+   :post [(s/valid? ::log %)]}
+  (Log. (conj (:entries log) data)))
+
+(defn read-index
+  [log index]
+  {:pre  [(s/valid? ::log log) (s/valid? int? index)]
+   :post [(s/valid? (s/or :data string? :not-found #{:not-found}) %)]}
+  (get (:entries log) (dec index) :not-found))
+
+(defn -read-index-next
+  [log _index]
+  {:pre  [(s/valid? ::log log) (s/valid? int? _index)]
+   :post [(s/valid? ::log %)]}
+  log)
+
+(defn last-index
+  [log]
+  {:pre  [(s/valid? ::log log)]
+   :post [(s/valid? int? %)]}
+  (count (:entries log)))
+
+(defn -last-index-next
+  [log]
+  {:pre  [(s/valid? ::log log)]
+   :post [(s/valid? ::log %)]}
+  log)
+
+(defn parameters
+  [op]
+  (case (:f op)
+    :append      [(-> op :value :topics) (-> op :value :data)]
+    :read        [(-> op :value :index)]
+    :status-last-index []))
+
+(defn ap
+  "Apply parameters."
+  [f x op]
+  (apply (partial f x) (parameters op)))
+
+(defn mock
+  "Given a pure model of the system and an operation, return a pure/mock response of
+  what we expect that the real system to respond with if we would execute said
+  operation against it."
+  [op log]
+  (case (:f op)
+    :append            (ap append      log op)
+    :read              (ap read-index  log op)
+    :status-last-index (ap last-index  log op)))
+
+(defn abstract-read
+  [value]
+  (if (= (:topics value) "membership")
+    (let [command (case (-> value :data :data :command)
+                    "add node"    "add"
+                    "remove node" "remove"
+                    (assert false
+                            (str "abstract-read: impossible, invalid value: " value)))]
+
+      (str "membership/" command " " (-> value :data :data :peer :name)))
+    (-> value :data)))
+
+(defn abstract
+  "Abstracts away unnessesary details from the real system's response."
+  [op]
+  (case (:f op)
+    :append      (or (-> op :value :indexes first)
+                     :unavailable)
+    :read        (-> op :value abstract-read)
+    :status-last-index (-> op :value)))
+
+(defn transition
+  [log op]
+  (case (:f op)
+    :append            (ap append-next       log op)
+    :read              (ap -read-index-next  log op)
+    :status-last-index (ap -last-index-next  log op)))
+
+(defn- mismatch
+  [op mock real]
+  (model/inconsistent
+   (with-out-str
+     (pprint {:op       (op/Op->map op)
+              :expected mock
+              :got      real}))))
+
+(defrecord LogModel [log]
+  Model
+  (step [_model op]
+
+    (let [mock (mock op log)
+          real (abstract op)]
+
+      (if (= mock real)
+        (->LogModel (transition log op))
+        (mismatch op mock real)))))
+
+(deftest wgl-iterator-error
+  (let [a (fn [] (analysis (LogModel. (initial-log))
+                           (ct/read-history-2 "data/wgl-error.edn")))]
+    (is (thrown-with-msg?
+         java.lang.AbstractMethodError
+         #"Method knossos/wgl/Op.iterator\(\)Ljava/util/Iterator; is abstract"
+         (a)))))
