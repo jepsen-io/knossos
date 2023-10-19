ce5b9d58ff2905a947510520b713d15531b4c1f2
Aphyr <aphyr@aphyr.com>
2023-10-18 21:33:00 -0500

    0.3.10, plus dep bumps

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject knossos "0.3.10-SNAPSHOT"
+(defproject knossos "0.3.10"
   :description "Linearizability checker"
   :url "https://github.com/aphyr/knossos"
   :license {:name "Eclipse Public License"
@@ -6,12 +6,12 @@
   :repositories {"sonatype-oss-public"
                  "https://oss.sonatype.org/content/groups/public/"}
   :main knossos.cli
-  :dependencies [[org.clojure/math.combinatorics "0.1.6"]
+  :dependencies [[org.clojure/math.combinatorics "0.2.0"]
                  [org.clojure/clojure "1.11.1"]
                  [potemkin "0.4.6"]
                  [slingshot "0.12.2"]
                  [interval-metrics "1.0.1"]
-                 [org.clojure/tools.cli "1.0.214"]
+                 [org.clojure/tools.cli "1.0.219"]
                  [com.boundary/high-scale-lib "1.0.6"]
                  [org.clojars.pallix/analemma "1.0.0"
                   :exclusions [org.clojure/clojure]]
