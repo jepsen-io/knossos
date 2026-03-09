9242b067e78fc414832ac6acab8af44ddb5e0d60
Kyle Kingsbury <aphyr@aphyr.com>
2026-03-09 17:59:25 -0500

    v0.3.15

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject knossos "0.3.15-SNAPSHOT"
+(defproject knossos "0.3.15"
   :description "Linearizability checker"
   :url "https://github.com/jepsen-io/knossos"
   :license {:name "Eclipse Public License"
